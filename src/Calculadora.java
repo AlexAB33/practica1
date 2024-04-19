412808f224c9230ae25d9038673f1b03fe13cc67
AlexAB33 <alejandro.arcos-bellido@iesruizgijon.com>
2024-04-19 12:29:34 +0200

    v1.3
    Se volvió a añadir el método de división

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -36,7 +36,9 @@ public class Calculadora {
     public void multiplica(double n1, double n2) {
         System.out.println("La multiplicación de " + n1 + " por " + n2 + " es " + (n1*n2));
     }
-    
+    public void divide(double n1, double n2) {
+        System.out.println("La división de " + n1 + " entre " + n2 + " es " + (n1/n2));
+    }
     
     // Getters y Setters (Source -> Insert code -> Getter and Setter)
 
